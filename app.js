b9bff9cea183f5dabfce842653132cf91593086c
Mohit Delhiwala <mohithem@buffalo.edu>
2018-11-03 18:27:31 -0400

    Added logic to lookup recipes based on the items provided

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const dbName = 'dudeimstaaarviing';
 
 const client = new MongoClient(url);
 
-const findRecipes = function (db, callback) {
-  const collection = db.collection('documents');
-  collection.find({}).toArray(function (err, docs) {
+const findRecipes = function (filter, db, callback) {
+  const collection = db.collection('recipes');
+  const f = filter || {};
+  collection.find(f).toArray(function (err, docs) {
+    callback(docs);
+  });
+}
+
+const findIngredients = function (filter, db, callback) {
+  const collection = db.collection('ingredients');
+  const f = filter || {};
+  collection.find(f).toArray(function (err, docs) {
     callback(docs);
   });
 }
@@ -15,9 +24,16 @@ const findRecipes = function (db, callback) {
 client.connect(function(err) {
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-  findRecipes(db, function(docs) {
+  findIngredients({ name: "butter" }, db, function(docs) {
     console.log("Found the following recipes");
     console.log(docs)
     client.close();
-  })
+  });
+
+  let ingredientsOnHand = ["olive oil", "salt", "pepper", "pears", "sweet potato noodles", "butter", "chives", "lemon"];
+  findRecipes({ ingredientsRequired: ingredientsOnHand }, db, function (docs) {
+    console.log("Found the following recipes for the ingredients given");
+    console.log(docs)
+    client.close();
+  });
 });
--- db-creator.js
+++ db-creator.js
@@ -4,26 +4,15 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'dudeimstaaarviing';
 
 const client = new MongoClient(url);
-const ingredientsList = [
-  "beef",
-  "bread",
-  "butter",
-  "cheese",
-  "chicken",
-  "eggs",
-  "garlic",
-  "green peppers",
-  "mexican cheese",
-  "milk",
-  "mozarella",
-  "onions",
-  "peanut butter",
-  "pepper",
-  "pepperjack",
-  "red peppers",
-  "salt",
-  "tomatoes",
-];
+const ingredientsList = ["beef", "bread", "butter", "cheese", "chicken", "eggs", "garlic",
+                        "green peppers", "mexican cheese", "milk", "mozarella", "onions",
+                        "peanut butter", "pepper", "pepperjack", "red peppers", "salt",
+                        "tomatoes", "olive oil", "pears", "sweet potato noodles", "chives", "lemon"];
+
+const recipesList = [{
+  name: "Roasted Sweet Potato Noodles with Pears",
+  ingredientsRequired: ["olive oil", "salt", "pepper", "pears", "sweet potato noodles", "butter", "chives", "lemon"]
+}];
 
 let ingredients = []
 for (let i = 0; i < ingredientsList.length; i++) {
@@ -33,10 +22,23 @@ for (let i = 0; i < ingredientsList.length; i++) {
   });
 }
 
+let recipes = []
+for (let i = 0; i < recipesList.length; i++) {
+  recipe = recipesList[i];
+  recipe['id'] = i + 1;
+  recipes.push(recipe);
+}
+
 const insertIngredients = function (db, callback) {
-  const collection = db.collection('documents');
+  const collection = db.collection('ingredients');
   collection.insertMany(ingredients, function (err, result) {
-    console.log("Inserted 18 documents into the collection");
+    callback(result);
+  });
+}
+
+const insertRecipes = function (db, callback) {
+  const collection = db.collection('recipes');
+  collection.insertMany(recipes, function (err, result) {
     callback(result);
   });
 }
@@ -46,6 +48,11 @@ client.connect(function(err) {
   const db = client.db(dbName);
   insertIngredients(db, function() {
     console.log("Inserted ingredients into the database successfully!");
+    client.close();
+  });
+
+  insertRecipes(db, function() {
+    console.log("Inserted recipes into the database successfully!");
     client.close();
   });
 });
